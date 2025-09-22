0378636241deea87f1a13379d9f09c48b7fc75f4
Abdalla Ahmed Jacob <abdallaahmedjacob@gmail.com>
2025-09-22 16:33:35 +0300

    feat: Add max and min in array program (25)\n\nThis program fills an array with random numbers and finds both the maximum and minimum values.

---
+++ 25_MaxAndMinInArray/25_MaxAndMinInArray.cpp
@@ -0,0 +1,73 @@
+/*
+* This program fills an array with random numbers and then finds and prints
+* both the maximum and minimum numbers in that array.
+* It demonstrates a fundamental algorithm for array analysis.
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Generates a random number within a specified range.
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+// Fills an array with a user-specified number of random integers.
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
+}
+
+// Prints all elements of an array to the console.
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+// Finds the maximum number in an array.
+// It assumes the array is not empty.
+int MaxNumberInArray(int Array[], int arrLength)
+{
+    int Max = Array[0];
+    for (int i = 1; i < arrLength; i++)
+    {
+        if (Array[i] > Max)
+            Max = Array[i];
+    }
+    return Max;
+}
+
+// Finds the minimum number in an array.
+// It assumes the array is not empty.
+int MinNumberInArray(int Array[], int arrLength)
+{
+    int Min = Array[0];
+    for (int i = 1; i < arrLength; i++)
+    {
+        if (Array[i] < Min)
+            Min = Array[i];
+    }
+    return Min;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr[100];
+    int arrLength;
+    FillArrayWithRandomNumbers(arr, arrLength);
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+    cout << "\nMax number = " << MaxNumberInArray(arr, arrLength) << endl;
+    cout << "\nMin number = " << MinNumberInArray(arr, arrLength) << endl;
+    return 0;
+}
